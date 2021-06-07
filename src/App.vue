051786e2d97a1eaf868d1aeae216b4a7e5903586
Justin Hager <developerjustinhager@pm.me>
2021-06-06 17:40:12 -0700

    end of Components pt 1

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,75 @@
 <template>
   <v-app>
-    <v-card width="400" class="mx-auto mt-5">
-      <v-card-title primary-title>
-        <h1 class="display-1">Login</h1>
-      </v-card-title>
-      <v-card-text>
-        <v-form>
-          <v-text-field
-            name="username"
-            label="Username"
-            prepend-icon="mdi-account-tie"
-          ></v-text-field>
-          <v-text-field
-            name="name"
-            label="Enter your password"
-            hint="At least 8 characters"
-            min="8"
-            placeholder="Password"
-            :type="showPassword ? 'text' : 'password'"
-            prepend-icon="mdi-lock"
-            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-            @click:append="showPassword = !showPassword"
-          ></v-text-field>
-          <v-divider></v-divider>
-          <v-card-actions>
-            <v-btn color="success">Register</v-btn>
-            <v-spacer></v-spacer>
-            <v-btn color="info">Login</v-btn>
-          </v-card-actions>
-          <v-card-actions>
-            <h2>Button Classes</h2>
-            <v-container>
-              <v-btn color="primary">primary</v-btn>
-              <v-btn color="secondary">secondary</v-btn>
-              <v-btn color="accent">accent</v-btn>
-              <v-btn color="success">success</v-btn>
-              <v-btn color="error">error</v-btn>
-              <v-btn color="warning">warning</v-btn>
-              <v-btn color="info">info</v-btn>
-            </v-container>
-          </v-card-actions>
-        </v-form>
-      </v-card-text>
-    </v-card>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>
+        Vuetify Dashboard
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text rounded>Home</v-btn>
+      <v-btn text rounded>Login</v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-card width="400" class="mx-auto mt-5">
+        <v-card-title primary-title>
+          <h1 class="display-1">Login</h1>
+        </v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              name="username"
+              label="Username"
+              prepend-icon="mdi-account-tie"
+            ></v-text-field>
+            <v-text-field
+              name="name"
+              label="Enter your password"
+              hint="At least 8 characters"
+              min="8"
+              placeholder="Password"
+              :type="showPassword ? 'text' : 'password'"
+              prepend-icon="mdi-lock"
+              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="showPassword = !showPassword"
+            ></v-text-field>
+            <v-divider></v-divider>
+            <v-card-actions>
+              <v-btn color="success">Register</v-btn>
+              <v-spacer></v-spacer>
+              <v-btn color="info">Login</v-btn>
+            </v-card-actions>
+            <v-card-actions>
+              <h2>Button Classes</h2>
+              <v-container>
+                <v-btn color="primary">primary</v-btn>
+                <v-btn color="secondary">secondary</v-btn>
+                <v-btn color="accent">accent</v-btn>
+                <v-btn color="success">success</v-btn>
+                <v-btn color="error">error</v-btn>
+                <v-btn color="warning">warning</v-btn>
+                <v-btn color="info">info</v-btn>
+              </v-container>
+            </v-card-actions>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </v-main>
+    <v-footer color="primary lighten-1" padless>
+      <v-row justify="center" no-gutters>
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          color="white"
+          text
+          rounded
+          class="my-2"
+        >
+          {{ link }}
+        </v-btn>
+        <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
+          {{ new Date().getFullYear() }} — <strong>Vuetify Dasboard</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
   </v-app>
 </template>
 
@@ -54,6 +81,7 @@ export default {
 
   data: () => ({
     showPassword: false,
+    links: ['Home', 'Login'],
   }),
 };
 </script>
