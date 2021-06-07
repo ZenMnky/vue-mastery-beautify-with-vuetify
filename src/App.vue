95e80cc78a147c1262437b2d4e44abb84b21b33f
Justin Hager <developerjustinhager@pm.me>
2021-06-06 17:08:05 -0700

    getting started with vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-card width="400" class="mx-auto mt-5">
+      <v-card-title primary-title>
+        <h1 class="display-1">Login</h1>
+      </v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field
+            name="username"
+            label="Username"
+            prepend-icon="mdi-account-tie"
+          ></v-text-field>
+          <v-text-field
+            name="name"
+            label="Enter your password"
+            hint="At least 8 characters"
+            min="8"
+            placeholder="Password"
+            :type="showPassword ? 'text' : 'password'"
+            prepend-icon="mdi-lock"
+            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            @click:append="showPassword = !showPassword"
+          ></v-text-field>
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-btn color="success">Register</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn color="info">Login</v-btn>
+          </v-card-actions>
+          <v-card-actions>
+            <h2>Button Classes</h2>
+            <v-container>
+              <v-btn color="primary">primary</v-btn>
+              <v-btn color="secondary">secondary</v-btn>
+              <v-btn color="accent">accent</v-btn>
+              <v-btn color="success">success</v-btn>
+              <v-btn color="error">error</v-btn>
+              <v-btn color="warning">warning</v-btn>
+              <v-btn color="info">info</v-btn>
+            </v-container>
+          </v-card-actions>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {},
+
+  data: () => ({
+    showPassword: false,
+  }),
+};
+</script>
